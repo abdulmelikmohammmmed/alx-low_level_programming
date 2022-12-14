6f2e704121e759963d0d09a2135571f7e55c605c
Abdulmelik Mohammed <mk22malika@gmail.com>
2022-12-14 10:50:59 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,26 @@
 #include "main.h"
 
 /**
- * _abs - return abs value
- * @n : number to check
- * Return: 0 or 1
+ * print_sign - function that prints the sign of a number..
+ * @n: the input number to check
+ * Return: int.
  */
 
-int _abs(int n)
+int print_sign(int n)
 {
-	return (n >= 0 ? n : n * -1);
-
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * jack_bauer - printsevery minute of the day of jack bauer
+ * Return:void
+ */
+
+void jack_bauer(void)
+{
+
+	int h1;
+	int h2;
+	int m1;
+	int m2;
+	int a = 9;
+
+	h2 = 0;
+	while (h2 <= 2)
+	{
