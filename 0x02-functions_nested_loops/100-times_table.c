38758f042ae95a60c7ba257cd223c435db7994e8
Abdulmelik Mohammed <mk22malika@gmail.com>
2022-12-14 13:33:20 -0800

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,9 +23,9 @@ void print_times_table(int n)
 				_putchar('0' + rep);
 			else if (rep < 10)
 			{
-				_puchar(' ');
-				_puchar(' ');
-				_puchar('0' + rep);
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + rep);
 			}
 			else if (rep < 100)
 			{
