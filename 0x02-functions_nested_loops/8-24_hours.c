7576ccc9d63c63af56f062076f9c5bfa6e49affd
Abdulmelik Mohammed <mk22malika@gmail.com>
2022-12-14 11:37:34 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,3 +17,29 @@ void jack_bauer(void)
 	h2 = 0;
 	while (h2 <= 2)
 	{
+		if (h2 == 2)
+		{
+			/*Restrain to 23h, not 29*/
+			a = 3;
+		}
+		h1 = 0;
+		while (m2 <= 5)
+		{
+			m1 = 0;
+			while (m1 <= 9)
+			{
+				_putchar('0' + h2);
+				_putchar('0' + h1);
+				_putchar(':');
+				_putchar('0' + m2);
+				_putchar('0' + m1);
+				_putchar('\n');
+				m1++;
+			}
+			m2++;
+		}
+		h1++;
+	}
+	h2++;
+}
+}
