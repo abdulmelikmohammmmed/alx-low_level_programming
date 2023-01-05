f7a4548d3d948f6524a0b3522b1be43a66de4c3c
Abdulmelik Mohammed <mk22malika@gmail.com>
2023-01-04 17:37:42 -0800

    task0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+#define MAIN_H
 #define MAIN_H
 
 void _puts_recursion(char *s);
@@ -10,4 +10,4 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-#endi
+#endif
